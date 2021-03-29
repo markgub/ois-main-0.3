48df4dbe378255489da6b0b4d3705b6951da4cfd
Mark-Kirill Gubal <55079821+markgub@users.noreply.github.com>
2021-03-29 12:17:43 +0300

    Camera page accesses the camera now

---
+++ OIS/Coordinator.swift
@@ -0,0 +1,27 @@
+//
+//  Coordinator.swift
+//  OIS
+//
+//  Created by User on 29.03.2021.
+//
+
+import SwiftUI
+
+import SwiftUI
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+  @Binding var isCoordinatorShown: Bool
+  @Binding var imageInCoordinator: Image?
+  init(isShown: Binding<Bool>, image: Binding<Image?>) {
+    _isCoordinatorShown = isShown
+    _imageInCoordinator = image
+  }
+  func imagePickerController(_ picker: UIImagePickerController,
+                didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+     guard let unwrapImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+     imageInCoordinator = Image(uiImage: unwrapImage)
+     isCoordinatorShown = false
+  }
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+     isCoordinatorShown = false
+  }
+}
--- OIS/LoginPageView.swift
+++ OIS/LoginPageView.swift
@@ -163,9 +163,25 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct PageCamera: View{
+    
+    @State var image: Image? = nil
+    @State var showCaptureImageView: Bool = false
+    
     var body: some View{
-        VStack{
-            Text("Kaamera leht")
+        ZStack{
+            VStack{
+                Button(action: {self.showCaptureImageView.toggle()}, label: {
+                    Text("Vali pilti")
+                })
+                image?.resizable()
+                    .frame(width: 250, height: 250)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            }
+            if (showCaptureImageView) {
+                CaptureImageView(isShown: $showCaptureImageView, image: $image)
+            }
         }
     }
 }
@@ -175,5 +191,28 @@ struct PageAR: View {
         VStack{
             Text("Liitrealsus")
         }
+    }
+}
+
+struct CaptureImageView {
+    /// MARK: - Properties
+   @Binding var isShown: Bool
+   @Binding var image: Image?
+   
+   func makeCoordinator() -> Coordinator {
+     return Coordinator(isShown: $isShown, image: $image)
+   }
+}
+extension CaptureImageView: UIViewControllerRepresentable {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CaptureImageView>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        //picker.sourceType = .camera
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController,
+            context: UIViewControllerRepresentableContext<CaptureImageView>) {
+        
     }
 }
